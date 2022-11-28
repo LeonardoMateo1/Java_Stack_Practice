0dcb930c2efc5246334ac867098db2d322be8103
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2022-11-28 16:05:33 -0500

    First assignment

---
+++ week_1/Program.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("My Name is Leonardo Mateo");
+        System.out.println("I am 21 years old");
+        System.out.println("My hometown is Naples, Florida");
+    }
+}
